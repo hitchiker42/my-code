11ea4bc911f150542a9021b0c5e7146c8dce2c26
Tucker <Hitchiker42@gmail.com>
2013-05-01 10:43:52 -0400

    update for arch

--- lisp/bin-utils.lisp
+++ lisp/bin-utils.lisp
@@ -1,6 +1,6 @@
 (defpackage :tucker.binutils
   (:use :cl :tucker.misc)
-  (:export 
+  (:export
    :define-binary-class
    :define-tagged-binary-class
    :define-binary-type
@@ -10,3 +10,14 @@
    :parent-of-type
    :current-binary-object
    :+null+))
+(defun as-keyword (sym)
+  "return keyword corrsponding to given symbol"
+  (intern (string sym) :keyword))
+(defun slot->defclass-slot (spec)
+  "create a class slot with name (car spec)"
+  (let ((name (car spec)))
+    `(,name :initarg ,(as-keyword name) :accessor ,name)))
+(defmacro define-binary-class  (name slots)
+  "create a defclass for name with given slots"
+  `(defclass ,name ()
+     ,(mapcar #'slot->defclass-slot slots)))
--- lisp/misc.lisp
+++ lisp/misc.lisp
@@ -9,12 +9,25 @@
 (defun seq (&key (start 0) (end 10) (step 1))
   "return a list of integers from start to end by step"
   (loop for i from start upto end by step collecting i))
-(defun interleave (list1 list2)
+#|(defun interleave (list1 list2)
   (let ((temp ())
         (a (copy-list list1))
         (b (copy-list list2)))
     (loop until (equal nil a) doing (push (pop a) temp) (push (pop b) temp))
-    (reverse temp)))
+    (reverse temp)))|#
+;;faster than the above imperitive version, same # of conses though
+(defun interleave (list1 list2)
+  (let* ((a (copy-list list1))
+        (b (copy-list list2)))
+    (labels ((acc (bool lst)
+               (if bool
+                   (if (car a)
+                       (acc nil (cons (pop a) lst))
+                       lst)
+                   (if (car b)
+                       (acc t (cons (pop b) lst))
+                       lst))))
+      (reverse (acc t ())))))
 (defmacro with-gensyms ((&rest names) &body body)
   `(let ,(loop for n in names collect `(,n (gensym)))
      ,@body))
@@ -28,3 +41,15 @@
   `(coerce ,vector 'list))
 (defmacro list->vector (list)
   `(coerce ,list 'vector))
+(defun rot (int n)
+  "rotate the n least significant bits of n to the most significant bits"
+  (let* ((len (integer-length int))
+         (last-bits (ldb (byte (- len n) n) int))
+         (first-bits (ldb (byte n 0) int))
+         (ans 0))
+    (setf (ldb (byte (- len n) 0) ans ) last-bits)
+    (setf (ldb (byte n (- len n)) ans) first-bits)
+    ans))
+(defmacro side-effect (&body forms)
+"for some reason forms needs to be written with 1 less layer of parens"
+  `(progn ,forms nil))
