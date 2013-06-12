2176d27e1b48e3eef8358c420b5f3028d8218405
Tucker <Hitchiker42@gmail.com>
2013-06-12 18:27:16 -0400

    added util.el, some useful elisp stuff

--- elisp/misc.el
+++ elisp/misc.el
@@ -1,4 +1,7 @@
-(require 'cl)
+(require 'cl-lib)
+;;actually useful
+(defun to-hex (dec)
+  (format "%x" dec))
 (cl-defmacro seq (&key (start 0) (end 10) (step 1))
 `(let ((seq-count ,start)
        (seq-list ()))
@@ -14,3 +17,32 @@
        (setq x (+ var 1)))))
 (defmacro body-test (&rest body)
   `(progn ,@body))
+;; char *decToHex(unsigned input)
+;; {
+;;     char *output = malloc(sizeof(unsigned) * 2 + 3);
+;;     strcpy(output, "0x00000000");
+
+;;     static char HEX[] = "0123456789ABCDEF";
+
+;;     // represents the end of the string.
+;;     int index = 9;
+
+;;     while (input > 0 ) {
+;;         output[index--] = HEX[(input & 0xF)];
+;;         input >>= 4;            
+;;     }
+
+;;     return output;
+;; }
+;; (defun to-hex (dec) 
+;; "This probably doesn't work as it would require big endian
+;;  input to work correctly"
+;;   (let ((i (length (format "%x" dec)))
+;;         (in dec)
+;;         (out 0))
+;;     (while (> in 0)
+;;       (setf out (+ out (* (expt 16 i) (logand in #xf))))
+;;       (setf in (lsh in 4))
+;;       (setf i (1- i)))
+;;     out))
+
