177a4b31cacc0f278353e88fa53ebe2756d80a4a
Tucker <Hitchiker42@gmail.com>
2014-04-14 15:42:55 -0400

    minor additions to readFile,c for prog6

--- cs520/prog6/readFile.c
+++ cs520/prog6/readFile.c
@@ -1,6 +1,9 @@
 //given a file name make a stream that reads the file
 //1000 bytes at a time counting the newlines and keeping a global
 //count
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
@@ -8,6 +11,61 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+typedef struct event_loop_data *event_loop_handle;
+typedef void (*handler_fn)(void *);
+typedef void*(produce_fn)(void*);
+typedef void*(init_fn)(void*);
+typedef struct stream_data *stream_handle;
+event_loop_handle create_event_loop();
+void cleanup_event_loop(event_loop_handle);
+void stop_event_loop(event_loop_handle);
+void announce_event(event_loop_handle,char *,void *);
+void register_event(event_loop_handle , const char *, handler_fn);
+void start_event_loop(event_loop_handle , handler_fn , void*);
+event_loop_handle create_event_loop();
+stream_handle create_stream(const char *,const char *,
+                            produce_fn ,init_fn,event_loop_handle);
+stream_handle create_stream(const char*,const char*,produce_fn,init_fn,
+                            event_loop_handle);
+int start_stream(stream_handle,void*);
+void cleanup_stream(stream_handle);
+static void *xmalloc (size_t n){
+  void *p = malloc (n);
+  if (!p && n != 0){
+    perror("out of memory");
+    exit(1);
+  }
+  return p;
+}
+
+/* Change the size of an allocated block of memory P to N bytes,
+   with error checking.  */
+/*
+static void *xrealloc (void *p, size_t n){
+  if (!n && p){
+    free (p);
+    return NULL;
+  }
+  p = realloc (p, n);
+  if (!p && n){
+    perror("out of memory");
+    exit(1);
+  }
+  return p;
+}
+*/
+static void *xcalloc (size_t s){
+  return memset(xmalloc (s), 0, s);
+}
+static void *xmemdup (void const *p, size_t s){
+  return memcpy (xmalloc (s), p, s);
+}
+
+/* Clone STRING.  */
+
+static char *xstrdup (char const *string){
+  return xmemdup (string, strlen (string) + 1);
+}
 #define MAX(a,b)                                \
   ({ __typeof__ (a) _a = (a);                   \
     __typeof__ (b) _b = (b);                    \
@@ -54,7 +112,7 @@ void *read_file_produce(void *client_data){
 }
 void read_file_end(stream_handle handle){
   munmap(data->mmaped_file,data->file_len);
-  xfree(handle);
+  cleanup_stream(handle);
 }
 //should this use return instead of exit?
 void *read_file_init(void *client_data){
@@ -70,5 +128,19 @@ void *read_file_init(void *client_data){
     perror("mmap failed");
     exit(1);
   }
-  return 1;
+  return data;
+}
+int main(int argc,char *argv[]){
+  if(argc < 2){
+    fprintf(stderr,"Error no file names given\n");
+    return 1;
+  }
+  event_loop_handle event_loop=create_event_loop();
+  read_file_data *data=alloca(sizeof(read_file_data));
+  
+  if(event_loop==NULL){
+    fprintf(stderr,"create_event_loop failed\n");
+    exit(1);
+  }
+  start_event_loop(event_loop,read_file_init,data);
 }
