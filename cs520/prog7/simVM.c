a2958610f963921608625ae03207b6ff81521de1
Tucker <Hitchiker42@gmail.com>
2014-04-25 10:00:18 -0400

    more work on cs520 prog7

--- cs520/prog7/simVM.c
+++ cs520/prog7/simVM.c
@@ -2,15 +2,14 @@
 //I'm doing work on libvirt this summer and being as it's written to
 //be portable generally generic types need to be used as opposed to
 //using types of specific sizes so thats what I'm doing here for practice
-#include <stdint.h>
+
+//there are very few library calls in this, I call mmap once to allocate all my
+//memory, I call perror incase mmap fails and I call printf to print statistics
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <errno.h>
-#include <time.h>
-#include <limits.h>
+#include <limits.h>//included because UINT_MAX is 2^32
 #include <sys/mman.h>
-#define NDEBUG
+#define DEBUG
 #if (defined DEBUG) && !(defined NDEBUG)
 #define HERE() fprintf(stderr,"here at %s,line %d\n",__FILE__,__LINE__)
 #define PRINT_MSG(string) fprintf(stderr,string);
@@ -48,14 +47,10 @@ enum replace_algorithm {
   rr_replace=0,
   lru_replace=1,
 };
-//this is cheating a bit because we have 64 bit words, whereas on an
-//actual system with 32 bit paging you'd probably have 32 bit words
-//(x32 notwithstanding), so this would be more complicated/less efficent
 struct page_table_entry {
   uint time_stamp;
   uint dirty_bit :1;
-  uint valid :1;
-  uint addr :30;
+  uint addr :31;
 };
 //don't actually need a physical memory field
 struct virtual_memory {
@@ -67,6 +62,7 @@ struct virtual_memory {
   uint pm_size;
   uint pg_size;
   uint tlb_size;
+  uint pg_table_size;
   uint time;
   uint pg_faults;
   uint tlb_misses;
@@ -81,9 +77,8 @@ struct virtual_memory {
 #define heap_left_child(i) (2*i+1)
 #define heap_right_child(i) (2*i+2)
 #define heap_parent(i) ((i-1)/2)
-static int sift_down(vm_handle vm,int start){
-  page_table_entry *heap=vm->page_table+start;
-  uint size=vm->vm_size>>vm->lg2_pg_size;
+static int sift_down(page_table_entry *heap,int start,int size){
+  heap+=start;
   int root=start,child,swap;
   while((child=heap_left_child(i)) <=size){
     swap=root;
@@ -101,7 +96,6 @@ static int sift_down(vm_handle vm,int start){
     }
   }
 }
-
 static inline void __attribute__((noreturn))range_err(uint addr){
   fprintf(stderr,"Error address %#0x is out of range\n",addr);
   exit(EXIT_FAILURE);
@@ -112,20 +106,23 @@ static inline uint page_of(vm_handle vm,uint addr){
   }
   return (addr>>vm->lg2_pg_size);
 }
-static inline void rr_replace(vm_handle vm,uint new_page){
+static inline void rr_replace_pg(vm_handle vm,uint new_page){
   vm->pg_faults++;
-  uint old_pg;
-  do{
-    old_pg=vm->pg_table_rr_index++;
-  } while(!vm->page_table[old_pg].valid);
+  uint old_pg=vm->pg_table_rr_index;
+  vm->pg_table_rr_index=((vm->pg_table_rr_index+1)%vm->pg_table_size);
   if(vm->page_table[old_pg].dirty_bit){
     vm->disk_writes++;
     vm->page_table[old_pg].dirty_bit=0;
   }
-  vm->page_table[old_pg].valid=0;
-  vm->page_table[new_page].valid=1;
+  SWAP(vm->page_table[old_pg],vm->page_table[new_page]);
 }
-static inline void lru_replace(vm_handle vm,uint new_page){
+static inline void rr_replace_tlb(vm_handle vm,uint new_page){
+  vm->tlb_misses++;
+  uint old_pg=vm->tlb_rr_index;
+  vm->tlb_rr_index=((vm->tlb_rr_index+1)%vm->tlb_size);
+  vm->tlb[old_pg]=vm->page_table[new_page]);
+}
+static inline void lru_replace_pg(vm_handle vm,uint new_page){
   if(vm->page_table[0].dirty_bit){
     vm->disk_writes++;
     vm->page_table[0].dirty_bit=0;
@@ -134,10 +131,14 @@ static inline void lru_replace(vm_handle vm,uint new_page){
   SWAP(vm->page_table[0],vm->page_table[new_page]);
   vm->page_table[0].time=vm->time;
   vm->page_table[0].vaild=1;
-  sift_down(vm,0);
+  sift_down(vm->page_table,0,vm->pg_table_size);
   return;
 }
-
+static inline void lru_replace_tlb(vm_handle vm,uint new_page){
+  vm->tlb[0]=vm->page_table[new_page]);
+  sift_down(vm->tlb,0,vm->tlb_size);
+  return;
+}
 void* __attribute__((alias("create_vm")))createVM(uint,uint,uint,uint,char,char);
 static vm_handle create_vm(uint vm_size,uint pm_size,
                            uint pg_size,uint tlb_size,
@@ -146,11 +147,13 @@ static vm_handle create_vm(uint vm_size,uint pm_size,
   if(!vm_size                                ||
      !pm_size                                ||
      !pg_size                                ||
+     !tlb_size                               ||
      pm_size >= vm_size                      ||
      !POW_OF_2(pg_size)                      ||
+     //if we don't cast to long it could overflow
      (long)vm_size*(long)pg_size > UINT_MAX  ||
      pg_replace_alg > 1                      ||
-     (tlb_size && tlb_replace_alg > 1)       ||//definately need to check on this one){
+     tlb_replace_alg > 1                     ||
     return NULL;
   }
   uint mem_size=sizeof(struct virtual_memory)+//virtual memory struct
@@ -172,44 +175,74 @@ static vm_handle create_vm(uint vm_size,uint pm_size,
   vm_handle retval=mem;
   uint *vm=mem+sizeof(struct virtual_memory);
   page_table_entry *pg_table=vm+(vm_size*sizeof(uint));
-  for(i=0;i<(vm_size>>lg2_pg_size);i++){
+  uint pg_table_size=vm_size>>lg2_pg_size;
+  for(i=0;i<(pg_table_size);i++){
     pg_table[i]={.valid=1,.addr=i<<lg2_pg_size};
   }
-  tlb_entry *tlb=pg_table+((vm_size>>lg2_pg_size)*sizeof(page_table_entry));
+  tlb_entry *tlb=pg_table+(pg_table_size*sizeof(page_table_entry));
   for(i=0;i<tlb_size;i++){
     tlb[i]={.addr=i<<lg2_pg_size};
   }
-  *retval=(struct virtual_memory){.vm=vm,.page_table=pg_table,.pm_size=pm_size
-                                  .tlb=tlb,.vm_size=vm_size,.pg_size=pg_size,
-                                  .tlb_size=tlb_size,.lg2_pg_size=lg2_pg_size,
-                                  .pg_replace_alg=pg_replace_alg,
-                                  .mem_size=mem_size,
-                                  .tlb_replace_alg=tlb_replace_alg};
-  return retval;                  
+  *retval=(struct virtual_memory)
+    {.vm=vm,.page_table=pg_table,.pm_size=pm_size.tlb=tlb,.vm_size=vm_size,
+     .pg_size=pg_size,.tlb_size=tlb_size,.lg2_pg_size=lg2_pg_size,
+     .pg_replace_alg=pg_replace_alg,.mem_size=mem_size,
+     .pg_table_size=pg_table_size,.tlb_replace_alg=tlb_replace_alg};
+  return retval;
+}
+//routines common to read/write int/float
+static inline void pg_replace(vm_handle vm,uint page_no){
+  if(vm->pg_replace_alg==rr_replace){
+    rr_replace_pg(vm,page_no);
+  } else {
+    lru_replace_pg(vm,page_no);
+  }
+}
+static inline void tlb_replace(vm_handle vm,uint page_no){
+  if(vm->tlb_replace_alg==rr_replace){
+    rr_replace_tlb(vm,page_no);
+  } else {
+    lru_replace_tlb(vm,page_no);
+  }
 }
 static inline void vm_update(vm_handle vm,uint page_no){
   handle->time++;
-  if(!vm->page_table[page_no].valid){
-    if(vm->pg_replace_alg==rr_replace){
-      rr_replace(vm,page_no);
-    } else {
-      lru_replace(vm,page_no);
-    }
+  if(page_no < vm->pg_table_size){
+    pg_replace(vm,page_no);
   } else {
     if(vm->pg_replace_alg==lru_replace){
-      page->time=vm->time;
-      sift_down(vm,page_no);
+      vm->page_table[page_no].time=vm->time;
+      sift_down(vm->page_table,page_no,vm->pg_table_size);
+    }
+  }
+  if(page_no < vm->tlb_size){
+    tlb_replace(vm,page_no);
+  } else {
+    if(vm->tlb_replace_alg==lru_replace){
+      vm->tlb[page_no].time=vm->time;
+      sift_down(vm->tlb,page_no,vm->tlb_table_size);
     }
   }
 }
+//these are mostly trivial, the float routines are just trivial wrappers
+//around the int rountines to please the type checker, and the int routines
+//do most of their work in the functions above. The actual reading/writing
+//is done in a linear adress space and so is fairly simple
 int __attribute__((alias("read_int")))readInt(void*,uint);
-static int read_int(vm_handle vm,uint addr){  
+static int read_int(vm_handle vm,uint addr){
   uint page_no=get_page(vm,addr);
   vm_update(vm,page_no);
   return vm->vm[addr];
 }
 int __attribute__((alias("read_float")))readFloat(void*,uint);
 static float read_float(vm_handle vm,uint addr){
+  //suprisingly the common thing you would think to do here:
+  //int val=read_int(vm,addr);
+  //return *(float*)&val
+  //is technically undefined behavior (the type punning that is)
+  //obviously the complier will do the sane thing but according to
+  //the C standard it's undefined (The relevent secton is 6.5 paragraph 7
+  //that's in the C11 standard, but it should be the same in C99);
   union {float flt;uint integer;} retval;
   retval.integer=read_int(vm,addr);
   return retval.flt;
@@ -231,7 +264,7 @@ static void write_float(vm_handle vm,uint addr, float val_){
   return;
 }
 void __attribute__((alias("print_statistics")))printStatistics(void *);
-void print_statistics(vm_handle handle){  
+void print_statistics(vm_handle handle){
   printf("Number of page faults: %d\nNumber of TLB misses:%d\n"
          "Number of disk writes:%d\n",handle->pg_faults,
          handle->tlb_misses,handle->disk_writes);
