babcebca51c67db43b6bf7d9bebccf17bdadc81c
Tucker <Hitchiker42@gmail.com>
2014-06-02 16:19:02 -0400

    more work on perl mpd client

--- perl/my_mpc.pl
+++ perl/my_mpc.pl
@@ -4,13 +4,45 @@ no warnings 'experimental';
 use autodie;
 use Socket;
 use Getopt::Long;
+use vars '$mpd_socket';#dynamicly scoped 
 Getopt::Long::Configure("gnu_getopt");
 my $host = "localhost";
 my $port = 6600;
-my $mpd_error_re='ACK \[(\d+)@\(d+)\] \{(\w+)\} (\w+)\n'
+#use of qr compiles the regex when the variable is defined, otherwise
+#the regex would be compiled each time it was used
+my $mpd_error_re=qr'ACK \[(\d+)@\(d+)\] \{(\w+)\} (\w+)\n';
+my $mpd_info_re=qr'([a-zA-Z]+): (.+)';
+my @mpd_commands=[
+     #query status
+    'clearerror','currentsong','idle','status','stats',
+    #playback options
+    'consume','crossfade','mixrampdb','mixrampdelay','random','repeat',
+    'setvol','single','replay_gain_mode','replay_gain_status','volume',
+    #controling playback
+    'next','pause','play','playid','previous','seek','seekid','seekcur','stop',
+    #current playlist
+    'add','addid','clear','delete','move','moveid','playlistfind',
+    'playlistinfo','playlistsearch','plchangesposid','prio','prioid',
+    'shuffle','swap','swapid','addtagid','cleartagid',
+    #stored playlist
+    'listplaylist','listplaylistinfo','listplaylists','load','playlistadd',
+    'playlistclear','playlistdelete','playlistmove','rename','rm','save',
+    #music data base
+    'count','find','findadd','list','listall','listallinfo','listfiles',
+    'lsinfo','readcomments','search','searchadd','searchaddpl',
+    'update','rescan',
+    #misc
+    'kill','close','ping','password','outputs','disableoutput',
+    'enableoutput','toggleoutput'
+];
+my %mpd_dispatch_table{@mpd_commands} =
+  map(\&gen_mpd_request_fun->($_),@mpd_commands);
+$mpd_dispatch_table{'prev'}=$mpd_dispatch_table{'previous'};
+
 sub getopt_long($,$,@){ #shortopts, longopts, options
     my($shortopts,$longopts,@args)=@_;
-    return split(/ /,strip(qx("getopt -u -o $shortopts --long $longopts -- @args")));
+    return
+      split(/ /,strip(qx"getopt -u -o $shortopts --long $longopts -- @args"));
 }
 sub socket_connect($,$){        #hostname,port
     my($host,$port)=@_;
@@ -26,9 +58,92 @@ sub socket_connect($,$){        #hostname,port
         return undef;
     }
 }
+#if I was using this as a more conventional function I'd have the socket handle
+#come first, but this way is eaiser
+sub mpd_request($,$,@){#command, socket_handle, args
+    my ($command,$socket_handle,@args) = @_;
+    print($socket_handle,$command,@args);
+    my (@responce,$line,$i);
+    while($line=readline($socket_handle)){
+        @responce[$i++]=chomp($line);
+    }
+    if($line =~ $mpd_error_re){
+        say(STDERR(),"mpd error in command ",$3,": ",$4);
+    } else {
+        return \@responce;
+    }
+}
+sub gen_mpd_request_fun($){#command
+    my $command=shift;
+    return sub{mpd_request($command,$mpd_socket,@_);}
+}
+sub check_opts($,$,$,$){
+    my ($numargs,$reqargs,$optargs,$restarg)=@_;
+    #insure than numargs is an acceptable number of arguments
+    #for a function with the prototype corrsponding to the
+    #number of required/optional/rest arguments specified
+    if($numargs<$reqargs || (!$restarg && ($numargs > $reqargs+$optargs))){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+sub mpd_connect($,$){#$hostname, $port
+    $mpd_socket = socket_connect(@_);
+    if(!$mpd_socket){
+        die (sprintf("Unable to connect to mpd serve at %d:%d",@_));
+    }
+    my $line=chomp(readline($mpd_socket));
+    if($line =~ /OK MPD [0-9.]+/){
+        return;
+    } else {
+        die (sprintf("Did not recieve responce from mpd server at %d:%d",@_));
+    }
+}
+#these two functions will only work for info on one song, or for the status command
+sub parse_mpdinfo($){#takes an array reference
+    my $aref=shift;
+    my %mpdinfo;
+    for my $info (@$aref) {
+        $info =~ $mpd_info_re;
+        $mpdinfo{$1}=$2;
+    }
+    return \%mpdinfo;
+}
+sub read_mpdinfo($){#takes a filehandle
+    my $fd=shift;
+    my %mpdinfo;
+    while(my $info=chomp(readline($fd))){
+        $info =~ $mpd_info_re;
+        $mpdinfo{$1}=$2;
+    }
+    return \%mpdinfo
+}
+sub parse_playlistinfo($){
+    my $fd=shift;
+    my @playlist; #array of hashes
+    my $i=0;
+    while(my $info=chomp(readline($fd))){
+        $info =~ $mpd_info_re;
+        $playlist[$i]->{$1}=$;2
+        if($1 eq 'Id'){
+            $i++;
+        }
+    }
+    return \@playlist;
+}
 
+            
+        
+    
 GetOptions('host|h=s' => \$host,
            'port|p=i' => \$port);
+given $ARGV[0]{
+    #don't take any arguments and give no intresting output
+    when(['next','prev','stop','clear','kill']){
+        $mpd_dispatch_table{$_}->();
+    }
+}
 
 =head1 MPD_SPEC
 
