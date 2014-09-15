6abbe42d0f2fe12d6f9349efe9a1540bfda659cd
Tucker <Hitchiker42@gmail.com>
2014-09-15 16:03:34 -0400

    cs homework

--- cs758/assignment1.c
+++ cs758/assignment1.c
@@ -6,7 +6,7 @@
 #include <limits.h>
 #include <unistd.h>
 #include <sys/time.h>
-long int random(void);
+long int random(void);11
 /* The various sorting algorithm names */
 static const char *counting_sort_str = "counting";
 static const char *radix_sort_str = "radix";
