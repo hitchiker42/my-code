d9f7ab4cad39dce29f96ad40d3257815baba8b26
Tucker <Hitchiker42@gmail.com>
2014-09-30 16:32:19 -0400

    Added file for math homework

--- cs758/asn5/trie.h
+++ cs758/asn5/trie.h
@@ -1,4 +1,4 @@
 typedef trie_node trie_node;
 typedef sparse_trie_node sparse_trie_node;
 #define upcase(c) (c & ~0x20)
-#define char_to_index(c) (upcase(c) - 0x40)
+#define char_to_index(c) (upcase(c) - 0x40)1
