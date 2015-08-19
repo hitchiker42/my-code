40d4c1b917456858af89f224103084089c9655d5
Tucker DiNapoli <T.DiNapoli42@gmail.com>
2015-08-19 11:29:03 -0400

    added C file with sorting algorithms

---
+++ C/readline_generic.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+static void do_readline(void(*f)(char*,void*),void *user_data,char *prompt){
+  if(!prompt){
+    prompt=">";
+  }
+  char *line_read=NULL;
+  while(1){
+    line_read=readline(prompt);
+    if(line_read){
+      if(*line_read){
+        add_history(line_read);
+      } else {continue;}
+    } else {fputs("\n",stderr);exit(0);}
+    f(line_read,user_data);
+    free(line_read);
+  }
+}
