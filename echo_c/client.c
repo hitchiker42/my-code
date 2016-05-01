e57489348939f0f06932ca3d9e3c68ee18b13a0e
Tucker DiNapoli <T.DiNapoli42@gmail.com>
2016-05-01 19:29:16 -0400

    more work on nework code in C

--- echo_c/client.c
+++ echo_c/client.c
@@ -1,9 +1,4 @@
 #include "echo.h"
-  //Bind the socket
-  if(bind(sock, BITCAST(&addr, struct sockaddr *), sizeof(addr)) < 0){
-    perror("bind");
-    return -1;
-  }
 int connect_client(const char *server, uint16_t port, double maxwait){
   //Create a sockaddr_in struct representing the desired server
   struct addrinfo *server_addr = getaddrbyname(server);
@@ -45,19 +40,16 @@ int connect_client(const char *server, uint16_t port, double maxwait){
   }
   return -1;
 }
-  
-void __attrtibute__(noreturn) run(const char *server, uint16_t port){
-  int sock = connect_client(server, port, 0);//Don't try more that once
-  if(sock < 0){
-    exit(EXIT_FAILURE);
-  }
+//Read lines from the interactive input 'in' until EOF,
+//send them to the server, read the same line back and write it to out
+void __attribute__(noreturn) echo_interactive(int sock, FILE *in, FILE *out){
   char *buf, *outbuf;
   size_t bufsz, outbufsz;
   ssize_t buflen;
   while((buflen = getline(&buf, &buflen, stdin)) > 0){
-    buflen -= 1;//ignore the newline
-    ssize_t nbytes = send(sock, buf, buflen-1);
-    if(nbytes != buflen-1){
+    //We need to send the newline to allow the server to detect the end of the message 
+    ssize_t nbytes = send(sock, buf, buflen, 0);
+    if(nbytes != buflen){
       fprintf(stderr, "Falied to write all bytes to socket\n");
       if(nbytes < 0){
         exit(EXIT_FAILURE);
@@ -67,11 +59,13 @@ void __attrtibute__(noreturn) run(const char *server, uint16_t port){
     }
     /*
       We could just reuse the buffer from getline, but that feels like cheating
-     */
+    */
     if(bufsz > outbufsz){
       xrealloc(outbuf, bufsz);
       outbufsz = bufsz;
     }
+    //Since we get back the same message we sent it's
+    //obviously going to be the same size
     nbytes = recv(sock, outbuf, buflen, MSG_WAITALL);
     //since we pass MSG_WAITALL it's an error if we don't read
     //all the bytes
@@ -79,9 +73,52 @@ void __attrtibute__(noreturn) run(const char *server, uint16_t port){
       fprintf(stderr, "Error reading from socket\n");
       exit(EXIT_FAILURE);
     }
+    //Remember we still have the newline, so no need to add another one
     fwrite(buf, 1, buflen, stdout);
-    fputs("\n", stdout);
   }
   exit(EXIT_SUCCESS);
+}
+//For each line in the regular file 'in', send it to the server,
+//get the same line back and write it to out.
+void __attribute__(noreturn) echo_file(int sock, FILE *in, FILE *out,
+                                       size_t insz){
+  int fd = fileno(in);
+  size_t bufsz = insz;
+  void *buf = mmap(NULL, insz, PROT_READ, MAP_PRIVATE, fd, 0);
+  if(buf == MAP_FAILED){
+    perror("mmap");
+    exit(EXIT_FAILURE);
+  }
+  ssize_t nbytes = send(sock, buf, bufsz, 0);
+  if(nbytes < bufsz){
+    perror("send");
+    exit(EXIT_FAILURE);
+  }
+    
+  void *buf = buf_start;
+  void *bufptr = buf;
+  size_t buflen = insz;
+  while((bufptr = memchr(buf, '\n', buflen))){
+    size_t msglen = bufptr - buf;
+    buflen -= msglen;
+    ssize_t nbytes = send(sock, buf, buflen, 0);    
+  
+  
+  
+  
+void __attrtibute__(noreturn) run(const char *server, uint16_t port,
+                                  FILE *in, FILE *out){
+  int sock = connect_client(server, port, 0);//Don't try more that once
+  if(sock < 0){
+    exit(EXIT_FAILURE);
+  }
+  struct stat buf;
+  fstat(fileno(in), &buf);
+  int isreg_in = S_ISREG(buf.st_mode);
+  if(isreg_in){
+    echo_file(sock, in, out, buf.st_size);
+  } else {
+    echo_interactive(sock, in, out);
+  }
 }
   
--- echo_c/server.c
+++ echo_c/server.c
@@ -1,2 +1,66 @@
 #include "echo.h"
 #include <pthread.h>
+static const size_t msgbufsz = 4096;
+//echo all lines in buf to sock, there must be at least one line in buf, which
+//starts at the start of buf and ends at line_end, line_end < buflen.
+//If is an unterminated line left in buf it is copied to the begining of buf and
+//its size is returned, otherwise 0 is returned.
+size_t echo_lines(int sock, char *buf, size_t buflen, size_t line_end){
+  char *bufptr = buf;
+  do {
+    send(sock, bufptr, line_end + 1, 0);
+    bufptr += (line_end+1);
+    char *newline = memchr(bufptr, '\n', buflen-(line_end+1));
+    if(!newline){break;}
+    line_end += newline - bufptr;//noop on the first iteration
+  } while(1);
+  if(buflen > line_end){
+    //copy any unused bytes to the begining of the buffer
+    memcpy(buf, buf+line_end, buflen - line_end);
+    return buflen - line_end;
+  } else {
+    return 0;
+  }
+}
+void handle_client(int sock){
+  //We read at most 4096 bytes at a time from the socket, if there are
+  //more than 4096 bytes than we store the bytes in buf until we've read
+  //the entire message.
+  size_t bufsz = msgbufsz;
+  size_t buflen = 0;
+  char *buf = xmalloc(bufsz);
+  char *msgbuf[msgbufsz];
+  while(1){
+    ssize_t nbytes = recv(sock, msgbuf, msgbufsz, 0);
+    //Not that anyone is going to read this exit status
+    if(nbytes < 0){
+      perror("recv");
+      pthread_exit(EXIT_FAILURE);
+    } else if(nbytes == 0){
+      pthread_exit(EXIT_SUCCESS);
+    }
+    //This probably does more copying that necessary
+    char *newline = memchr(msgbuf, '\n', msgbufsz);
+    if(newline){
+      if(buflen != 0){
+
+
+int init_server(const char *hostname, uint16_t port){
+  struct addrinfo *server_addr = getaddrbyname(server);
+  if(!server_addr){
+    return -1;
+  }
+  struct sockaddr_in addr = {.sin_family = AF_INET,
+                            .sin_port = htons(port),
+                            .sin_addr = BITCAST(server_addr->ai_addr, struct in_addr)};
+  if((sock = socket(PF_INET, SOCK_STREAM, 0/*TCP*/)) < 0){
+    perror("socket");
+    return -1;
+  }
+  //Bind the socket
+  if(bind(sock, BITCAST(&addr, struct sockaddr *), sizeof(addr)) < 0){
+    perror("bind");
+    return -1;
+  }
+  return sock;
+}
