7626ed79a730cdf79b6208e1876524ca15ecdf18
Tucker DiNapoli <t.dinapoli42@gmail.com>
2016-06-04 04:30:30 -0400

    Cleaned up/orginized util.rkt

--- scheme/racket/util.rkt
+++ scheme/racket/util.rkt
@@ -31,12 +31,20 @@
       (acc (1+ var)))))
 (define-syntax-rule (until test body ...)
   (while (not test) body ...))
+;;A form of while without break/continue support, should be faster
+(define-syntax-rule (while-do test body1 body2 ...)
+  (do () ((not test) #f) body1 body2 ...))
+(define-syntax-rule (do-while test body1 body2 ...)
+  (begin
+    body1 body2 ...
+    (do () ((not test) #f) body1 body2 ...)))
 ;;Due to internal definitions you can't conditionally define a top level
 ;;symbol using normal if stmts, this is about as close as you can get to that
 (define-macro (static-if cond then . else)  
   (if cond
       `(begin ,then)
       `(begin ,@else)))
+
 ;;Returns the result of body, if any exceptions are raised they
 ;;are caught, the context of the exception is escapes and the result
 ;;of calling f with the exception is returned
@@ -44,8 +52,13 @@
   (with-caught-exceptions (lambda (exn) #f) body ...))
 (define-syntax-rule (return-exceptions body ...)
   (with-caught-exceptions (lambda (exn) exn) body ...))
-(define-syntax-rule (false-if-exception body ...)
-   (with-caught-exceptions (lambda (exn) #f) body ...)))
+
+;;Basically this tries to set var to itself, which raises an exception
+;;if var insn't defined. We catch that exception and then set var to expr
+;;if var is already defined we set var to itself which is a no-op
+(define-macro (define-once var expr)
+  `(define ,var (or (false-if-exception ,var) ,expr)))
+
 (define-macro (type-case expr . clauses)
   (let ((val (gensym)))
     `(let ((,val ,expr))
@@ -64,6 +77,7 @@
                      (list* 'else (cdr x))
                      (list* (list (car x) val)
                             (cdr x)))) clauses)))))
+;;TODO: rewrite using a syntax parameter, then write other anaphoric macros
 (define-syntax (aif stx)
   (with-syntax ((it (datum->syntax stx 'it)))
     (syntax-case stx ()
@@ -74,6 +88,16 @@
       ((_ cond then else rest ...)
        #'(let ((it cond))
            (if it then (begin else rest ...)))))))
+;;other macros
+(define-macro (concat-lit . args) (apply string-append args))
+;;I took this from the racket reference, it just appends a ? to an identifier
+;;which, given a type, generally gives a type predicate
+(define-syntax (make-pred stx)
+  (syntax-case stx ()
+    ((_ name)
+     (format-id #'name "~a?" (syntax-e #'name)))))
+
+;;;Data manipulation
 ;;TODO: look at set! transformers and try and use those here
 (define-syntax-rule (pop! ls)
   (prog1 (cl-car ls) (set! ls (cl-cdr ls))))
@@ -109,61 +133,6 @@
   (let ((ret (1- place)))
     (set! place ret)
     ret))
-
-;;A form of while without break/continue support, should be faster
-(define-syntax-rule (while-do test body1 body2 ...)
-  (do () ((not test) #f) body1 body2 ...))
-(define-syntax-rule (do-while test body1 body2 ...)
-  (begin
-    body1 body2 ...
-    (do () ((not test) #f) body1 body2 ...)))
-
-(define-macro (concat-lit . args) (apply string-append args))
-
-;;since we have format-symbol I probably don't need this
-(define-macro (build-symbol . args)
-  `(string->symbol
-    (string-join
-    (map (lambda (x) (my-if (symbol? x) (symbol->string x) x))
-         (list ,@args)) "")))
-
-;; (define-for-syntax (module-ref mod var)
-;;   (namespace-variable-value var (module->namespace mod)))
-;; (begin-for-syntax
-;;  (define-macro (module-ref mod var)
-;;   `(namespace-variable-value ',var (module->namespace ',mod))))
-;; (begin-for-syntax
-;;  (define-macro (module-ref mod var)
-;;   `(namespace-variable-value ',var (module->namespace ',mod))))
-;;I took this from the racket reference, it just appends a ? to an identifier
-;;which, given a type, generally gives a type predicate
-(define-syntax (make-pred stx)
-  (syntax-case stx ()
-    ((_ name)
-     (format-id #'name "~a?" (syntax-e #'name)))))
-
-;; (begin-for-syntax
-;;  (define-syntax (symbol-bound? stx)
-;;   (syntax-case stx ()
-;;     ((_ sym)
-;;      (if (identifier-binding #'sym) (syntax #t) (syntax #f))))))
-
-
-
-;; (begin-for-syntax
-;;  (define-syntax (here stx)
-;;     #`(list (quote-source-file #,stx)
-;;             (quote-line-number #,stx)
-;;             (quote-column-number #,stx)
-;;             (quote-character-position #,stx)
-;;             (quote-character-span #,stx))))
-;; (define-macro (assert! expr)
-;;   `(unless ,expr
-;;      (raise (make-exn:fail:assertion-simple
-;;              (format #f "Assertation-failure: ~s~%~s"
-;;                      'expr ,(here))
-;;              (current-continuation-marks)))))
-
 (define (hash->alist ht)
   (hash-map ht
             (lambda (key value)
@@ -199,23 +168,6 @@
 ;;a list is created if one dosent't
 (define (hash-push! ht key value)
   (hash-update! ht key (lambda (x) (cons value x)) null))
-;;define-once is way more complicated that it should be
-;;this is a kinda lame way to do this, but I can't think of another way
-;;since you can't define a top level variable inside of a conditional
-(define-macro (define-once var expr)
-  `(define ,var (or (false-if-exception ,var) ,expr)))
-  ;; (let ((boundp (false-if-exception (or var (not var)))))
-  ;;   (if boundp
-  ;;       `(define ,var ,var)
-  ;;       `(define ,var ,expr))))
-  ;; (syntax-rules ()
-  ;;   ((_ name body)
-  ;;    (define name (if (symbol-bound? name) (symbol-ref 'name) body)))
-  ;;   ((_ (name args ...) body1 body2 ...)
-  ;;    (define (name args ...)
-  ;;      (if (symbol-bound? name) ((symbol-ref 'name) args ...)
-  ;;          (begin body1 body2 ...))))))
-
 (define (string->char str)
   (string-ref str 0))
 (define (char->string char)
@@ -227,17 +179,6 @@
   (let ((f (lambda (x) (plumber-flush-handle-remove! x);;proobably unecessary
                    (apply proc args))))
     (plumber-add-flush! (current-plumber) f)))
-;; (define-macro (regexp-bind pat input . body)
-;;   `(let ((matched (regexp-match ,pat ,input)))
-;;      (unless (null? matched)
-;;        (when (< (length matched) 10)
-;;          (set! (matched (append matched
-;;                                 (make-list (- 10 (length matched)) #f)))))
-;;        (match matched
-;;          ((list-rest $@ $0 $1 $2 $3 $4 $5 $6 $7 $8 $9 $0)
-;;(define internal-xorshift-state (make-bytevector 16))
-;;(define xorshift-rand ((state internal-xorshift-state))
-;;         (for-syntax (all-from-out racket/base racket/string racket/syntax))
-;;         (for-meta 2 (all-from-out racket/base)))
+
 (provide (all-defined-out)
          (all-from-out "init.rkt"))
